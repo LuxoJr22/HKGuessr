eb9906c6166277bad1e9f2521b31dfa983297a24
LuxoJr22 <mathisbodu83@gmail.com>
2025-02-01 12:54:07 +0100

    init

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<video autoplay muted loop id="myvideo">
+	<source src="/assets/hk.mp4" type="video/mp4">
+</video>
+<div class="ui">
+	<div class="title">
+		<img src="/guessr.png">
+	</div>
+	<div class="selection">
+		<img src="/assets/sel.png">
+		<a class="btn text" id="launch" href="/game">Play</a>
+		<img src="/assets/seli.png">
+	</div>
+</div>
+
+<style>
+
+.ui {
+	position: absolute;
+	height: 80%;
+	width: 100%;
+	top: 0px;
+	display:flex;
+	justify-content: space-evenly;
+	flex-direction: column;
+	align-items: center;
+}
+
+a.text {
+	color: white;
+	font-size: 50px;
+	font-family: "Trajan";
+	text-decoration: none;
+}
+
+a.text:hover {
+	text-shadow: 0 0px 4px white;
+}
+
+.title {
+	width: 40%;
+	height: auto;
+}
+
+div.title > img {
+	width: 100%;
+	height: auto;
+}
+</style>
---
+++ myapp/src/routes/game/+page.svelte
@@ -0,0 +1,612 @@
+<div class="feature_image" id="place">
+	<img id="point">
+  </div>
+  <div class="top_ui" id="top">
+	<span class="text" id="Score">Score: 0</span>
+	<span class="text" id="Timer">TIMER</span>
+	<span class="text" id="round">Round : 0/5</span>
+  </div>
+  <div class="guess_ui" id="guess">
+	<div class="map_ui" id ="map">
+		<div class="zoom_outer" id="mapping">
+			<div id="tag" hidden>
+				<img src="/assets/knight.png">
+			</div>
+			<div id="tagpoint" hidden>
+				<img src="/Wayward_Compass.webp">
+			</div>
+			<svg height="100%" width="100%" id="box">
+				<line x1="0" y1="0" x2="400" y2="200" stroke-dasharray="4" id="line" />
+			</svg>
+			<div id="zoom">
+				<img src="/assets/map.png" alt="zoom">
+			</div>
+		</div>
+	</div>
+	<div class="mybutton">
+		<div class="guessimgs" id="guess_text">
+			<img src="/assets/guessbtnl.png">
+			<img src="/assets/guessbtn.png" class="img-top" id="btn">
+		</div>
+	</div>
+  </div>
+  <div class="bot_ui" id="bot">
+	<span class="text" id="Act_score">Score: 0</span>
+	<div class="nextimgs" id="next_text">
+		<img src="/assets/Nextl.png">
+		<img src="/assets/Next.png" class="img-top" id="next">
+	</div>
+	<div class="nextimgs" id="playa_text">
+		<img src="/assets/playal.png">
+		<img src="/assets/playa.png" class="img-top" id="playa">
+	</div>
+	<span class="text" id="Act_round">Round : 1/5</span>
+  </div>
+<script src="game.js">
+    import { onMount } from "svelte";
+	onMount(() => { (async () => {
+	var scale = 1,
+	totalSeconds,
+	panning = false,
+	pointX = 0,
+	pointY = 0,
+	start = { x: 0, y: 0 },
+	pos = {x: 0, y: 0},
+	tagpos = {x: 0, y: 0},
+	point = {x: 0, y: 0},
+	pospoint = {x:0, y:0},
+	tagpoint = {x:0, y:0},
+	score = 0,
+	Round = 1,
+	scene = 0,
+	gamepoints = [],
+	mappoints = [],
+	maptags = [],
+	tags = [],
+	points = [],
+	lines = [],
+	totalsec = 0,
+	chrono,
+	bot = document.getElementById("guess"),
+	bott = document.getElementById("bot"),
+	map = document.getElementById("map"),
+	act_score = document.getElementById("Act_score"),
+	act_round = document.getElementById("Act_round"),
+	zoom = document.getElementById("zoom"),
+	locate = document.getElementById("point"),
+	taupe = document.getElementById("top"),
+	tag = document.getElementById("tag"),
+	tagpo = document.getElementById("tagpoint"),
+	place = document.getElementById("place"),
+	timer = document.getElementById("Timer"),
+	button = document.getElementById("btn"),
+	scoretxt = document.getElementById("Score"),
+	round = document.getElementById("round"),
+	guess_imgs = document.getElementById("guess_text"),
+	next = document.getElementById("next_text"),
+	playa = document.getElementById("playa_text"),
+	line = document.getElementById("line"),
+	box = document.getElementById("box"),
+	rect = zoom?.getBoundingClientRect();
+
+	function get_rect()
+	{
+		pointX = 0;
+		pointY = 0;
+		start = { x: 0, y: 0 };
+		pos = {x: 0, y: 0};
+		scale = 1;
+		zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+		rect = zoom.getBoundingClientRect();
+	}
+
+
+	function loadmap()
+	{
+		scene = 0;
+		clearInterval(chrono);
+		StartTimer();
+		fetch('/map.json')
+			.then((response) => response.json())
+			.then((json) => {
+				var i = json.maps.length;
+				var o = Math.floor(Math.random() * i);
+				while (gamepoints.includes(o))
+				{
+					o = (o + 1) % i;
+				}
+				gamepoints.push(o);
+				var p = json.maps[o];
+				locate.src = p.link;
+				pospoint = p.coords});
+		scoretxt.innerHTML = "Score : " + score;
+		round.innerHTML = "Round : " + Round + "/5";
+	}
+
+	function reloadmap()
+	{
+		loadmap();
+		bot.style.right = 20 + "px";
+		bot.style.top = "";
+		bot.style.bottom = 80 + "px";
+		map.style.opacity = 0.5;
+		map.style.height = "auto";
+		map.style.width = 600 + "px";
+		tagpos.x = 0;
+		tagpos.y = 0;
+		tag.style.left = tagpos.x + "px";
+		tag.style.top = tagpos.y + "px";
+		setTransform()
+		tag.hidden = true;
+		tagpo.hidden = true;
+		taupe.style.display = "flex";
+		guess_imgs.style.display = "flex";
+		bott.style.display = "none";
+		box.style.display = "none"
+		get_rect();
+	}
+
+	function guess_scene()
+	{
+		totalsec += 300 - totalSeconds;
+		scene = 1;
+		bot.style.right = 0 + "px";
+		bot.style.top = 0 + "px";
+		map.style.opacity = 1;
+		map.style.height = 90 + "%";
+		map.style.width = "auto";
+		get_rect();
+		tagpos.x *= ((rect.right - rect.left) / 600);
+		tagpos.y *= ((rect.bottom - rect.top) / 390.96875);
+		tag.style.left = tagpos.x + "px";
+		tag.style.top = tagpos.y + "px";
+		tag.style.transform = "translate(" + 0 + "px, " + 0 + "px)";
+		tagpo.style.transform = "translate(" + 0 + "px, " + 0 + "px)";
+		tagpoint.x = pospoint.x * ((rect.right - rect.left) / 600);
+		tagpoint.y = pospoint.y * ((rect.bottom - rect.top) / 390.96875);
+		tagpo.style.left = tagpoint.x + "px";
+		tagpo.style.top = tagpoint.y + "px";
+		tagpo.hidden = false;
+		locate.src = "";
+		maptags.push({x: tagpos.x, y: tagpos.y});
+		mappoints.push({x: tagpoint.x, y: tagpoint.y});
+		if (tagpos.x == 0 && tagpos.y == 0)
+		{
+			tagpos.x = tagpoint.x;
+			tagpos.y = tagpoint.y;
+		}
+		let mid = {x: (tagpoint.x + tagpos.x) / 2, y: (tagpoint.y + tagpos.y) / 2};
+		pointX = (rect.right - rect.left) / 2 - mid.x;
+		pointY = (rect.bottom - rect.top) / 2 - mid.y - 225;
+		setTransform();
+		box.style.display = "block"
+		taupe.style.display = "none";
+		guess_imgs.style.display = "none";
+		bott.style.display = "flex";
+		clearInterval(chrono);
+		
+	}
+
+	function end_scene()
+	{
+		scene = 2;
+		scale = 1;
+		pointX = 0;
+		pointY = 0;
+		let i = 0;
+		console.log(maptags);
+		while (i < 4)
+		{
+			lines.push(line.cloneNode(true));
+			tags.push(tag.cloneNode(true));
+			tags[i].style.left = maptags[i].x + "px";
+			tags[i].style.top = maptags[i].y + "px";
+			document.getElementById("mapping").appendChild(tags[i]);
+			points.push(tagpo.cloneNode(true))
+			points[i].style.left = mappoints[i].x + "px";
+			points[i].style.top = mappoints[i].y + "px";
+			document.getElementById("mapping").appendChild(points[i]);
+			lines[i].setAttribute('x1', maptags[i].x);
+			
+			lines[i].setAttribute('y1', maptags[i].y);
+			lines[i].setAttribute('x2', mappoints[i].x);
+			lines[i].setAttribute('y2', mappoints[i].y);
+			document.getElementById("box").appendChild(lines[i]);
+			if (maptags[i].x == 0 && maptags[i].y == 0)
+			{
+				tags[i].hidden = true;
+				lines[i].style.display = "none";
+			}
+			else
+				tags[i].hidden = false;
+			i ++;
+		}
+		setTransform();
+		act_score.innerHTML = "Score : " + score;
+		let seconds = totalsec % 60;
+		let secondsTens = Math.floor(seconds / 10);
+		let secondsOnes = seconds % 10;
+		let minutes = Math.floor(totalsec / 60);
+		act_round.innerHTML = "" + minutes + ":" + secondsTens + secondsOnes;
+		next.style.display = "none";
+		playa.style.display = "inline-block";
+	}
+
+	loadmap();
+
+	place.style.maxHeight = window.innerHeight + "px";
+
+	window.onresize = function(event) {
+		place.style.maxHeight = window.innerHeight + "px";
+		place.style.maxWidth = window.innerWidth + "px";
+		rect = zoom.getBoundingClientRect();
+	}
+
+	function setTransform() {
+		zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+		var p = {x: pointX + tagpos.x * (scale - 1), y: pointY + tagpos.y * (scale - 1)}
+		line.setAttribute('x1', tagpos.x + p.x);
+		line.setAttribute('y1', tagpos.y + p.y);
+		tag.style.transform = "translate(" + p.x + "px, " + p.y + "px)";
+		p = {x: pointX + tagpoint.x * (scale - 1), y: pointY + tagpoint.y * (scale - 1)}
+		tagpo.style.transform = "translate(" + p.x + "px, " + p.y + "px)";
+		line.setAttribute('x2', tagpoint.x + p.x);
+		line.setAttribute('y2', tagpoint.y + p.y);
+		if (scene == 2)
+		{
+			let i = 0;
+			while ( i < 4)
+			{
+				p = {x: pointX + maptags[i].x * (scale - 1), y: pointY + maptags[i].y * (scale - 1)};
+				tags[i].style.transform = "translate(" + p.x + "px, " + p.y + "px)";
+				lines[i].setAttribute('x1', maptags[i].x + p.x);
+				lines[i].setAttribute('y1', maptags[i].y + p.y);
+				p = {x: pointX + mappoints[i].x * (scale - 1), y: pointY + mappoints[i].y * (scale - 1)};
+				points[i].style.transform = "translate(" + p.x + "px, " + p.y + "px)";
+				lines[i].setAttribute('x2', mappoints[i].x + p.x);
+				lines[i].setAttribute('y2', mappoints[i].y + p.y);
+				i ++;
+			}
+		}
+	}
+
+	function StartTimer() {
+		totalSeconds = 300;
+
+		chrono = setInterval(Timer_Tick , 1000);
+		let seconds = totalSeconds % 60;
+		let secondsTens = Math.floor(seconds / 10);
+		let secondsOnes = seconds % 10;
+		let minutes = Math.floor(totalSeconds / 60);
+		timer.innerHTML = "" + minutes + ":" + secondsTens + secondsOnes;
+	}
+
+	function Timer_Tick() {
+		if (totalSeconds > 0)
+		{
+			totalSeconds--;
+			let seconds = totalSeconds % 60;
+			let secondsTens = Math.floor(seconds / 10);
+			let secondsOnes = seconds % 10;
+			let minutes = Math.floor(totalSeconds / 60);
+
+			timer.innerHTML = "" + minutes + ":" + secondsTens + secondsOnes;
+		}
+		else
+		{
+			guess_scene();
+		}
+	}
+
+
+	zoom.onmouseleave = function(e)
+	{
+		panning = false;
+	}
+
+	button.onmousedown = function(e)
+	{
+		let scoring
+		if (tagpos.x > pospoint.x - 5 && tagpos.x < pospoint.x + 5 && tagpos.y > pospoint.y - 5 && tagpos.y < pospoint.y + 5)
+			scoring = 1000;
+		else
+		{
+			let dist = Math.sqrt((tagpos.x - pospoint.x) ** 2 + (tagpos.y - pospoint.y) ** 2) * 10;
+			if (dist > 1000)
+				dist = 1000;
+			scoring = Math.round(1000 - dist);
+		}
+		act_score.innerHTML = "Score : " + scoring;
+		act_round.innerHTML = "Round : " + Round + "/5";
+		score += scoring;
+		Round ++;
+		console.log(tagpos);
+		guess_scene();
+	}
+
+	zoom.onmousedown = function (e) {
+		e.preventDefault();
+		pos.x = (e.clientX - rect.left);
+		pos.y = (e.clientY - rect.top);
+		start = { x: pos.x - pointX, y: pos.y - pointY };
+		point.y = pos.y;
+		point.x = pos.x;
+		panning = true;
+	}
+
+	zoom.onmouseup = function (e) {
+		panning = false;
+		pos.x = (e.clientX - rect.left);
+		pos.y = (e.clientY - rect.top);
+		
+		if (point.y == pos.y && point.x == pos.x && scene == 0)
+		{
+			tag.hidden = false;
+			tagpos.x = (pos.x - pointX) / scale;
+			tagpos.y = (pos.y - pointY) / scale;
+			tag.style.left = tagpos.x + "px";
+			tag.style.top = tagpos.y + "px";
+			setTransform();
+		}
+	}
+
+	playa.onmousedown = function (e) {
+		document.location.reload();
+	}
+
+	next.onmousedown = function (e) {
+		if (Round < 6)
+			reloadmap();
+		else
+			end_scene();
+			
+	}
+
+	zoom.onmousemove = function (e) {
+		e.preventDefault();
+		if (!panning) {
+		return;
+		}
+		pos.x = (e.clientX - rect.left);
+		pos.y = (e.clientY - rect.top);
+		pointX = (pos.x - start.x);
+		pointY = (pos.y - start.y);
+		setTransform();
+	}
+
+	zoom.onwheel = function (e) {
+		e.preventDefault();
+		pos.x = (e.clientX - rect.left);
+		pos.y = (e.clientY - rect.top);
+		var xs = (pos.x - pointX) / scale,
+		ys = (pos.y - pointY) / scale,
+		delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+		(delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+		if (scale < 1)
+			scale = 1;
+		pointX = pos.x - xs * scale;
+		pointY = pos.y - ys * scale;
+
+		setTransform();
+	}
+})();
+});
+</script>
+
+<style>
+
+#zoom {
+	width: 100%;
+	height: 100%;
+	transform-origin: 0px 0px;
+	transform: scale(1) translate(0px, 0px);
+	cursor: grab;
+	z-index: 1;
+	position: relative;
+}
+#tag {
+	z-index: 5;
+	position: absolute;
+	pointer-events: none;
+	overflow: visible;
+	
+}
+
+div#tag > img {
+	z-index: 21;
+	position: relative;
+	right: 25px;
+	bottom: 35px;
+	max-width: 50px;
+}
+
+#tagpoint {
+	z-index: 3;
+	position: absolute;
+	pointer-events: none;
+	overflow: visible;	
+}
+
+#line {
+	stroke:white;
+	stroke-width:2;
+}
+
+#box {
+	z-index: 2;
+	position: absolute;
+	pointer-events: none;
+	display: none;
+}
+
+#playa_text {
+	display: none;
+}
+
+div#tagpoint > img {
+	z-index: 20;
+	position: relative;
+	right: 25px;
+	bottom: 25px;
+	max-width: 50px;
+}
+
+.bot_ui {
+	z-index: 3;
+	position: absolute;
+	width: 100%;
+	bottom: 20px;
+	height: auto;
+	display:flex;
+	justify-content: space-evenly;
+	display: none;
+	overflow: visible;
+}
+
+.top_ui {
+	z-index: 3;
+	position: absolute;
+	width: 100%;
+	top: 30px;
+	/*left: 50%;*/
+	display:flex;
+	justify-content: space-evenly;
+
+}
+
+.nextimgs {
+	width: auto;
+	height: auto;
+	position: relative;
+	top: -20px;
+	display: inline-block;
+}
+
+.nextimgs .img-top {
+	display: none;
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: 99;
+}
+.nextimgs:hover .img-top {
+	display: inline;
+	filter: drop-shadow(0 0 2px white)
+}
+
+div.nextimgs > img {
+	filter: drop-shadow(0 0 5px black);
+	width: 300px;
+	height: auto;
+}
+
+/*#Score {
+
+}*/
+/*#round {
+	right:20px;
+	position: absolute;
+}
+#Timer {
+	right:50%;
+	position: absolute;
+}*/
+
+span.text {
+	color: white;
+	font-size: 35px;
+	font-family: "Trajan";
+	/*text-shadow: 0 0px 10px black;*/
+}
+
+.mybutton {
+	display: flex;
+  	justify-content: center;
+  	align-items: center;
+	width: 600px;
+}
+
+.guessimgs {
+	width: auto;
+	height: auto;
+	position: relative;
+	display: inline-block;
+}
+.guessimgs .img-top {
+	display: none;
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: 99;
+}
+.guessimgs:hover .img-top {
+	display: inline;
+}
+
+div.guessimgs > img {
+	filter: drop-shadow(0 0 5px black);
+	width: 300px;
+	height: auto;
+}
+
+
+div#zoom > img {
+	width: 100%;
+	height: 100%;
+}
+
+.feature_image {
+	z-index: 0;
+    height: 1080px;
+    width: 1920px;
+	position: relative;
+}
+
+.guess_ui {
+	position: absolute;
+	bottom: 80px;
+	right: 20px;
+}
+
+.map_ui {
+	opacity: 0.5;
+	background-color: white;
+	width: 600px;
+	height: auto;
+	padding: 1px;
+	transition: transform .2s;
+	transform: scale(1.0);
+}
+
+.feature_image img{
+	position: relative;
+	height: 100%;
+    width: auto;
+	display: block;
+	margin-left: auto;
+ 	margin-right: auto;
+}
+
+.zoom_outer {
+	background-color: rgb(0, 0, 0);
+	width: auto;
+	height: auto;
+}
+
+.map_ui:hover {
+	opacity: 1.0 !important;
+	transform: scale(1.0);
+}
+/*.zoom_outer {
+	padding: 50px;
+	background-color: rgb(0, 102, 0);
+	transition: transform .2s;
+	width: 200px;
+	height: 200px;
+	margin: 0 auto;
+  }*/
+  
+  /*.zoom_outer:hover {
+	transform: scale(1.5);
+ }*/
+</style>
